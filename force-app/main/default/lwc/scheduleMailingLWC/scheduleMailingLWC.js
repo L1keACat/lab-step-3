5ddf7dff73ba919704515f3dc8ad76ab5f5dc3ef
L1keACat <36240624+L1keACat@users.noreply.github.com>
2022-07-27 20:36:42 +0300

    final commit

--- force-app/main/default/lwc/scheduleMailingLWC/scheduleMailingLWC.js
+++ force-app/main/default/lwc/scheduleMailingLWC/scheduleMailingLWC.js
@@ -1,13 +1,15 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
 import runBatchOnce from '@salesforce/apex/MailingSchedulerController.runBatchOnce';
 import scheduleBatch from '@salesforce/apex/MailingSchedulerController.scheduleBatch';
 import abortBatch from '@salesforce/apex/MailingSchedulerController.abortBatch';
+import checkSchedulerStatus from '@salesforce/apex/MailingSchedulerController.checkSchedulerStatus';
 
 export default class ScheduleMailingLWC extends LightningElement {
     buttonClicked;
     batchName = 'BirthdayBatch';
     schedulerName = 'BirthdayEmailScheduler';
     cronExp = '';
+    isScheduled = false;
 
     get cssClass(){
         return this.buttonClicked ? 'slds-float_right slds-button slds-button_destructive slds-var-m-around_xx-small' : 'slds-float_right slds-button slds-button_brand slds-var-m-around_xx-small';
@@ -17,14 +19,42 @@ export default class ScheduleMailingLWC extends LightningElement {
         return this.buttonClicked ? 'Abort Batch' : 'Schedule Batch';
     }
 
+    @wire(checkSchedulerStatus, {schedulerName: '$schedulerName'})
+    status({data, error}) {
+        if(data) {
+            console.log(data);
+            this.isScheduled = data.status;
+            this.buttonClicked = this.isScheduled;
+            if (this.isScheduled) {
+                this.cronExp = data.cronexp;
+            }
+        }
+        if (error) {
+            console.log('An error has occurred:');
+            console.log(error);
+        }
+    }
+
     handleCronChange(event){
         this.cronExp = event.target.value;
     }
 
-    scheduleBatch() { 
-        this.buttonClicked = !this.buttonClicked;
+    toggleBatch() { 
         console.log(this.cronExp);
-        if (this.buttonClicked) {
+        if (!this.buttonClicked) {
+            this.scheduleBatch();
+        } else {
+            this.abortBatch();
+        }
+    }
+
+    scheduleBatch() {
+        const isInputsCorrect = [...this.template.querySelectorAll('lightning-input')]
+            .reduce((validSoFar, inputField) => {
+                inputField.reportValidity();
+                return validSoFar && inputField.checkValidity();
+            }, true);
+        if (isInputsCorrect) {   
             scheduleBatch({
                 cronExp: this.cronExp,
                 batchName: this.batchName,
@@ -32,15 +62,19 @@ export default class ScheduleMailingLWC extends LightningElement {
             }).then(result => {
                 console.log(result);
             })
-        } else {
-            alert('Abort');
-            abortBatch({
-                schedulerName: this.schedulerName
-            }).then(result => {
-                alert('Aborted');
-                console.log(result);
-            })
+            this.buttonClicked = !this.buttonClicked;
         }
+    }
+
+    abortBatch() {
+        abortBatch({
+            schedulerName: this.schedulerName
+        }).then(result => {
+            console.log(result);
+            if (result == 'Job has been aborted successfully.') {
+                this.buttonClicked = !this.buttonClicked;
+            }
+        })
     }
 
     runOnce() { 
